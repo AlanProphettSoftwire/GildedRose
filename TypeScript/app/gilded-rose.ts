504123d950fb05cefc33be529b15f6461b7ba342
AlanProphettSoftwire <alan.prophett@softwire.com>
2025-09-25 11:04:58 +0100

    fix code formatting

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,54 +19,49 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name === 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name === "Sulfuras, Hand of Ragnaros") {
         continue;
       }
 
       this.items[i].sellIn = this.items[i].sellIn - 1;
 
       switch (this.items[i].name) {
-        case 'Backstage passes to a TAFKAL80ETC concert':
+        case "Backstage passes to a TAFKAL80ETC concert":
           if (this.items[i].sellIn < 0) {
-            this.items[i].quality = 0
+            this.items[i].quality = 0;
             break;
           }
 
           if (this.items[i].sellIn >= 10) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-          else if (this.items[i].sellIn >= 5) {
-            this.items[i].quality = this.items[i].quality + 2
-          }
-          else {
-            this.items[i].quality = this.items[i].quality + 3
+            this.items[i].quality = this.items[i].quality + 1;
+          } else if (this.items[i].sellIn >= 5) {
+            this.items[i].quality = this.items[i].quality + 2;
+          } else {
+            this.items[i].quality = this.items[i].quality + 3;
           }
 
           if (this.items[i].quality > 50) {
-            this.items[i].quality = 50
+            this.items[i].quality = 50;
           }
           break;
-        case 'Aged Brie':
+        case "Aged Brie":
           if (this.items[i].sellIn < 0) {
             this.items[i].quality = Math.min(this.items[i].quality + 2, 50);
-          }
-          else {
+          } else {
             this.items[i].quality = Math.min(this.items[i].quality + 1, 50);
           }
           break;
-        case 'Conjured Mana Cake':
+        case "Conjured Mana Cake":
           if (this.items[i].sellIn < 0) {
             this.items[i].quality = Math.max(0, this.items[i].quality - 4);
-          }
-          else {
+          } else {
             this.items[i].quality = Math.max(0, this.items[i].quality - 2);
           }
           break;
         default:
           if (this.items[i].sellIn < 0) {
             this.items[i].quality = Math.max(0, this.items[i].quality - 2);
-          }
-          else {
+          } else {
             this.items[i].quality = Math.max(0, this.items[i].quality - 1);
           }
           break;
