975df004612fce60ba8bf4a047ad80a25eb60568
AlanProphettSoftwire <alan.prophett@softwire.com>
2025-09-25 10:48:19 +0100

    implement logic for conjured mama cake Co-authored-by: tmgls <tmgls@users.noreply.github.com>

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -25,44 +25,51 @@ export class GildedRose {
 
       this.items[i].sellIn = this.items[i].sellIn - 1;
 
-      if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].sellIn < 0) {
-          this.items[i].quality = 0
-          continue;
-        }
+      switch (this.items[i].name) {
+        case 'Backstage passes to a TAFKAL80ETC concert':
+          if (this.items[i].sellIn < 0) {
+            this.items[i].quality = 0
+            break;
+          }
 
-        if (this.items[i].sellIn >= 10) {
-          this.items[i].quality = this.items[i].quality + 1
-        }
-        else if (this.items[i].sellIn >= 5) {
-          this.items[i].quality = this.items[i].quality + 2
-        }
-        else {
-          this.items[i].quality = this.items[i].quality + 3
-        }
+          if (this.items[i].sellIn >= 10) {
+            this.items[i].quality = this.items[i].quality + 1
+          }
+          else if (this.items[i].sellIn >= 5) {
+            this.items[i].quality = this.items[i].quality + 2
+          }
+          else {
+            this.items[i].quality = this.items[i].quality + 3
+          }
 
-        if (this.items[i].quality > 50) {
-          this.items[i].quality = 50
-        }
-        continue;
-      }
-
-
-      if (this.items[i].name === 'Aged Brie') {
-        if (this.items[i].sellIn < 0) {
-          this.items[i].quality = Math.min(this.items[i].quality + 2, 50);
-        }
-        else {
-          this.items[i].quality = Math.min(this.items[i].quality + 1, 50);
-        }
-        continue;
-      }
-
-      if (this.items[i].sellIn < 0) {
-        this.items[i].quality = Math.max(0, this.items[i].quality - 2);
-      }
-      else {
-        this.items[i].quality = Math.max(0, this.items[i].quality - 1);
+          if (this.items[i].quality > 50) {
+            this.items[i].quality = 50
+          }
+          break;
+        case 'Aged Brie':
+          if (this.items[i].sellIn < 0) {
+            this.items[i].quality = Math.min(this.items[i].quality + 2, 50);
+          }
+          else {
+            this.items[i].quality = Math.min(this.items[i].quality + 1, 50);
+          }
+          break;
+        case 'Conjured Mana Cake':
+          if (this.items[i].sellIn < 0) {
+            this.items[i].quality = Math.max(0, this.items[i].quality - 4);
+          }
+          else {
+            this.items[i].quality = Math.max(0, this.items[i].quality - 2);
+          }
+          break;
+        default:
+          if (this.items[i].sellIn < 0) {
+            this.items[i].quality = Math.max(0, this.items[i].quality - 2);
+          }
+          else {
+            this.items[i].quality = Math.max(0, this.items[i].quality - 1);
+          }
+          break;
       }
     }
     return this.items;
